126e7e6a9fe02bdaa5cbbd27797e0ed14046c68c
Richard Lee <dlackty@gmail.com>
2014-07-20 18:25:49 +0800

    Print debug output of rack env

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,7 +1,8 @@
 class QuotesController < Rulers::Controller
   def a_quote
     "There is nothing either good or bad " +
-      "but thinking makes it so."
+      "but thinking makes it so." +
+      "\n<pre>\n#{env}\n</pre>"
   end
 
   def exception
